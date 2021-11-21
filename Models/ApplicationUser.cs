156af24d284c905a3952c88b31b0a1fd7881d625
Propulus <jhribar8@gmail.com>
2021-11-21 21:53:05 +0100

    DELA!

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace web.Models
     public class ApplicationUser : IdentityUser
     {
         public int ClientId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
--- Models/Sensor.cs
+++ Models/Sensor.cs
@@ -7,12 +7,12 @@ namespace web.Models
     {
         //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int SensorId { get; set; }    
-        public ApplicationUser Owner { get; set; }
+        public ApplicationUser? Owner { get; set; }
 
         public string SerialNumber { get; set; }
-        public string Location { get; set; }
-        public string Type { get; set; }
-        public string FirmwareVersion { get; set; }
+        public string? Location { get; set; }
+        public string? Type { get; set; }
+        public string? FirmwareVersion { get; set; }
 
         public ICollection<SensorAccess> Accesses { get; set; } 
     }
