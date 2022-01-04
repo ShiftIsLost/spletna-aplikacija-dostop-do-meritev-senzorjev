ab626d12aaac49e8abe0fed13d90e2ae3457263e
propulus <jhribar8@gmail.com>
2022-01-04 14:31:33 +0100

    api auth

--- Controllers/Api/SensorApiController.cs
+++ Controllers/Api/SensorApiController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web.Data;
 using web.Models;
+using web.Filters;
 
 namespace web.Controllers_Api
 {
     [Route("api/v1/sensor")]
     [ApiController]
+    [ApiKeyAuth]
     public class SensorApiController : ControllerBase
     {
         private readonly AccessContext _context;
